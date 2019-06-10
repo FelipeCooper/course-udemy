35b3a5b4bc38ef5d6eb55c65563a14a4052ac25d
FelipeCooper <hoonter69@gmail.com>
2019-06-10 17:40:21 -0300

    exception2-java resolved--add try again

--- exceptions2-java/src/application/Program.java
+++ exceptions2-java/src/application/Program.java
@@ -7,11 +7,20 @@ import model.entities.Account;
 import model.exception.DomainExceptions;
 
 public class Program {
-
 	public static void main(String[] args) {
 		try {
+		inputDate();			
+		}
+		catch (DomainExceptions e) {
+			System.out.print(e.getMessage());
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Input error, try again");
+			inputDate();
+		}
+	}
+	public static void inputDate() throws DomainExceptions {
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.print("Name: ");
 		String name = sc.next();
 		System.out.print("Number account: ");
@@ -20,23 +29,11 @@ public class Program {
 		double balance = sc.nextDouble();
 		System.out.print("WhithDraw limite: ");
 		double whithdrawlimit = sc.nextDouble();
-		
 		Account account = new Account(name,whithdrawlimit,number,balance);
-		
 		System.out.print("Enter amount for whithdraw: ");
 		account.whithdraw(sc.nextDouble());
-		
 		System.out.printf("New Balance R$ %.2f", account.getBalance());
-		
-		
 		sc.close();
-		}
-		catch (DomainExceptions e) {
-			System.out.print(e.getMessage());
-		}
-		catch (InputMismatchException e) {
-			System.out.print("Input error");
-		}
 	}
 
 }
--- exceptions2-java/src/model/exception/DomainExceptions.java
+++ exceptions2-java/src/model/exception/DomainExceptions.java
@@ -1,6 +1,6 @@
 package model.exception;
 
-public class DomainExceptions extends Exception{
+public class DomainExceptions extends RuntimeException{
 
 	private static final long serialVersionUID = 5785659827843573580L;
 
